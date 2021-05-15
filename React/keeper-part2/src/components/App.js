7c683fd5fa8dde4c39a8f5fc2cef8211f67426c2
wafeliciano20 <firstwillow2@gmail.com>
2021-05-15 18:01:09 -0400

    Sucsessfully completed the challenge with the addition of this files

--- React/keeper-part2/src/components/App.js
+++ React/keeper-part2/src/components/App.js
@@ -2,14 +2,27 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
+import notes from '../notes';
 
 const App = () => {
     return <div>
         <Header />
-        <Note />
+        {notes.map(note =>
+            <Note 
+            key ={note.id}
+            title={note.title} 
+            content = {note.content}
+            />
+        )}
         <Footer />
 
     </div>;
 };
 
 export default App;
+
+/*
+    first I need to add the props to Note.js
+    Then loop over the notes array with map and 
+    create the Note components
+*/
--- React/keeper-part2/src/components/Note.js
+++ React/keeper-part2/src/components/Note.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Note = () => {
-    let title = "This is the note title";
-    let content ="This is the note content";
+const Note = (props) => {
+    // let title = "This is the note title";
+    // let content ="This is the note content";
 
     return (
         <div className="note">
-            <h1>{title}</h1>
-            <p>{content}</p>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
         </div>
     );
 }
