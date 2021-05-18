c5948b6371d9dbf13b2f3957d54c313654a5fbf1
wafeliciano20 <firstwillow2@gmail.com>
2021-05-17 23:20:40 -0400

    final files after compleated todo app

--- React/spead-operator-practice-todo-app/src/components/App.js
+++ React/spead-operator-practice-todo-app/src/components/App.js
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useState}  from "react";
+import ListItem from "./ListItem";
+
 
 function App() {
+
+  const [inputText,setInputText] = useState("");
+
+  const [todos, setTodos] = useState([]);
+
+  const handleChange = (event) => {
+    const value  = event.target.value;
+
+    setInputText(value);
+  }
+
+  const addTodo = () => {
+    setTodos( previousTodos => {
+      return [...previousTodos,inputText];
+    });
+
+    setInputText("");
+  }
+
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={addTodo}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {/* {todos.map( todoItem => <li>{todoItem}</li> ) } */}
+          {todos.map( todoItem => <ListItem key={todos.indexOf(todoItem)} value={todoItem} />) }
         </ul>
       </div>
     </div>
@@ -22,3 +45,9 @@ function App() {
 }
 
 export default App;
+
+//CHALLENGE: Make this app work by applying what you've learnt.
+//1. When new text is written into the input, its state should be saved.
+//2. When the add button is pressed, the current data in the input should be
+//added to an array.
+//3. The <ul> should display all the array items as <li>s
---
+++ React/spead-operator-practice-todo-app/src/components/ListItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ListItem = (props) =>  <li>{props.value}</li>;
+    
+ 
+
+
+export default ListItem;
